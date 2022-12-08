0f36446aba64e395413894242cd637cfc7f0a0be
nikoi4 <r.nicolascaif@gmail.com>
2022-12-08 08:59:10 -0300

    Adding Auth API TOKEN on basic view and its corresponding config in settings

--- eureka_stock_api/api/views.py
+++ eureka_stock_api/api/views.py
@@ -1,7 +1,18 @@
-from rest_framework.decorators import api_view
+from rest_framework.authentication import (
+    TokenAuthentication,
+    BasicAuthentication
+)
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 
 @api_view(["GET"])
+@authentication_classes([TokenAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
 def hello_world(request, *args, **kwargs):
     return Response({"message": "Hello world"})
