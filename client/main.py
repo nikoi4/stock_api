f21353c975057521e84e7a2974876bdad6113a74
nikoi4 <r.nicolascaif@gmail.com>
2022-12-08 16:33:25 -0300

    Adding new url endpoint to test client

--- client/main.py
+++ client/main.py
@@ -3,36 +3,54 @@ from getpass import getpass
 
 
 OPTIONS = {
-    '1': ['get/retrieve token', 'api-token-auth/'],
-    '2': ['get hello world', 'hello/'],
+    '1': ['create user and get token', 'register/'],
+    '2': ['retrieve token', 'api-token-auth/'],
+    '3': ['get hello world', 'hello/'],
     '0': ['Exit', 'quit']
 }
 
 
 def run():
-    # base_docker_api_endpoint = 'http://0.0.0.0:8000/api/v1/'
-    base_docker_api_endpoint = 'http://localhost:8080/api/v1/'
+    # base endpoint for docker
+    base_api_endpoint = 'http://0.0.0.0:8000/api/v1/'
+    # base endpoint for local
+    # base_api_endpoint = 'http://localhost:8080/api/v1/'
     print('-'*35)
     print('*** Welcome to Stock Market API ***')
     print('-'*35)
+
     auth_token = None
+
     while True:
         for option in OPTIONS.items():
             num = option[0]
             option_text = option[1][0]
             print('{} - {}'.format(num, option_text))
         user_option = input("\nSelect api action\n")
-        selected_option = OPTIONS[user_option][1]
-        if selected_option == OPTIONS['0'][1]:
+        if user_option == '0':
             print('See you soon!!')
             break
-        endpoint = '{}{}'.format(base_docker_api_endpoint, selected_option)
-        if selected_option == OPTIONS['1'][1]:
-            user = input('User Name:   ')
-            password = getpass('User Password:   ')
-            data = {'username': user, 'password': password}
+        if not OPTIONS.get(user_option, ''):
+            print('Invalid option')
+
+        selected_option = OPTIONS[user_option][1]
+        endpoint = '{}{}'.format(base_api_endpoint, selected_option)
+        if user_option in ['1', '2']:
+            if user_option == '1':
+                data = {
+                    'username': input('User Name: '),
+                    'email': input('Email: '),
+                    'first_name': input('User First Name: '),
+                    'last_name': input('User Last Name: '),
+                    'password': getpass('User Password: ')
+                }
+            if user_option == '2':
+                data = {
+                    'username': input('User Name: '),
+                    'password': getpass('User Password: ')
+                }
             response = requests.post(endpoint, json=data)
-            if response.status_code == 200:
+            if response.status_code in [200, 201]:
                 auth_token = response.json().get('token', '')
         else:
             headers = {'Authorization': 'Token {}'.format(auth_token)}
