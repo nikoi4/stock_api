d3071ef10a7d67fc5b4d7b43e4f9716e5a754ff6
nikoi4 <r.nicolascaif@gmail.com>
2022-12-10 23:22:53 -0300

    [PATCH] adding config for logger

--- eureka_stock_api/eureka_stock_api/settings.py
+++ eureka_stock_api/eureka_stock_api/settings.py
@@ -112,6 +112,45 @@ REST_FRAMEWORK = {
     }
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    # custom formatting to add datetime + level + content
+    'formatters': {
+        'timestamp': {
+            'format': '{asctime} {levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': f'{BASE_DIR}/my_log.log',
+            'formatter': 'timestamp',
+        }
+    },
+    'loggers': {
+        # Identifies custom logging
+        '': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        # identifies system wide logging
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
